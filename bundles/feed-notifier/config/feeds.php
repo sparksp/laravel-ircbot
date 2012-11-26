1412b4ca217c36f3e73f084a663696e13406ce97
Phill Sparks <me@phills.me.uk>
2012-11-26 13:48:07 +0000

    Update feeds. Add Google Alerts, Stack Overflow and Twitter. Remove GitHub.

--- bundles/feed-notifier/config/feeds.php
+++ bundles/feed-notifier/config/feeds.php
@@ -7,9 +7,30 @@ return array(
 		'channel' => '#laravel',
 	),
 
-	'laravel-github' => array(
-		'message' => '(:title) (:link)',
-		'url' => 'https://github.com/LaravelBot.private.atom?token=a6798a59cc4ebefb3415dc938a6ec1c2',
+	'laravel-google' => array(
+		'message' => 'Google Alert: (:title) (:link)',
+		'url' => 'http://www.google.com/alerts/feeds/15062750671952302504/1492468193374197150',
+		'channel' => '#laravel',
+		'clean_link' => function($url)
+		{
+			$parts = parse_url($url);
+			$query = parse_str($parts['query']);
+
+			if (isset($query['q'])) return $query['q'];
+
+			return $url;
+		},
+	),
+
+	'laravel-stackoverflow' => array(
+		'message' => 'Stack Overflow: (:title) (:link)',
+		'url' => 'http://stackoverflow.com/feeds/tag/laravel',
+		'channel' => '#laravel',
+	),
+
+	'laravel-twitter' => array(
+		'message' => 'New tweet by (:title) (:link)',
+		'url' => 'http://search.twitter.com/search.atom?q=laravel%20laravelphp%20-RT%20-%40laravel&show_user=true&rpp=3',
 		'channel' => '#laravel',
 	),
 
