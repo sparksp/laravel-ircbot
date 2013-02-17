aed9d6eb8b9762364db0ccb07fa79b8ae51b0be8
Brian Retterer <bretterer@gmail.com>
2013-02-16 20:56:32 -0500

    add ugt trigger to general triggers

--- bundles/irc-general/config/triggers.php
+++ bundles/irc-general/config/triggers.php
@@ -4,6 +4,7 @@ return array(
 
 		'!dataja'		=> "Don't ask to ask, Just ask!",
 		'!wysiwyg'		=> "What you see is what you get",
-		'!drama'		=> "I just can't take it anymore...."
+		'!drama'		=> "I just can't take it anymore....",
+		'!ugt'			=> "It is always morning when someone comes into a channel. We call that Universal Greeting Time http://www.total-knowledge.com/~ilya/mips/ugt.html"
 	
 );
