a82329302f028446de71f3e51707c43a54d708af
Daniel Smith <Bulk70@gmail.com>
2013-03-28 13:55:48 +0000

    Update emoticon because machuga told me to

--- bundles/irc-general/config/triggers.php
+++ bundles/irc-general/config/triggers.php
@@ -9,5 +9,5 @@ return array(
     '!help'         => "Before asking for help, use paste.laravel.com to provide 1. ALL relevant code (controllers, routes, models, views, anything) 2. expected and actual behaviour 3. any error messages you're getting. Thank you!",
     '!paste'		=> "You may paste your code at http://paste.laravel.com/",
     '!l4install'    => "Everything you need to know about installing and updating Laravel 4: http://niallobrien.me/2013/03/installing-and-updating-laravel-4/",
-    '!tableflip'    => "(╯°□°）╯︵ ┻━┻",
+    '!tableflip'    => "(╯°□°)╯︵ ┻━┻",
 );
