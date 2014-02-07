2c6bce8b3dcb86dddcd43c67e886406265b96397
Phill Sparks <me@phills.me.uk>
2014-02-07 00:01:39 +0000

    Update feeds.php
    Disable Forums and Twitter as these are no longer available sources.

--- bundles/feed-notifier/config/feeds.php
+++ bundles/feed-notifier/config/feeds.php
@@ -1,25 +1,17 @@
 <?php
 return array(
 
-	'laravel-forum' => array(
-		'message' => 'Forum Activity: (:title) (:link)',
-		'url' => 'http://forums.laravel.com/extern.php?action=feed&type=rss',
-		'channel' => '#laravel',
-	),
+	// The forums have moved!
+	// 'laravel-forum' => array(
+	// 	'message' => 'Forum Activity: (:title) (:link)',
+	// 	'url' => 'http://forums.laravel.com/extern.php?action=feed&type=rss',
+	// 	'channel' => '#laravel',
+	// ),
 
 	'laravel-google' => array(
 		'message' => 'Google Alert: (:title) (:link)',
 		'url' => 'http://www.google.com/alerts/feeds/15062750671952302504/1492468193374197150',
 		'channel' => '#laravel',
-		'clean_link' => function($url)
-		{
-			$parts = parse_url($url);
-			$query = parse_str($parts['query']);
-
-			if (isset($query['q'])) return $query['q'];
-
-			return $url;
-		},
 	),
 
 	'laravel-stackoverflow' => array(
@@ -28,11 +20,12 @@ return array(
 		'channel' => '#laravel',
 	),
 
-	'laravel-twitter' => array(
-		'message' => 'New tweet by (:title) (:link)',
-		'url' => 'http://search.twitter.com/search.atom?q=laravel%20laravelphp%20-RT%20-%40laravel&show_user=true&rpp=3',
-		'channel' => '#laravel',
-	),
+	// Twitter no longer has an atom api
+	// 'laravel-twitter' => array(
+	// 	'message' => 'New tweet by (:title) (:link)',
+	// 	'url' => 'http://search.twitter.com/search.atom?q=laravel%20laravelphp%20-RT%20-%40laravel&show_user=true&rpp=3',
+	// 	'channel' => '#laravel',
+	// ),
 
 	// This feed doesn't validate :(
 	// 'laravel-bundles' => array(
